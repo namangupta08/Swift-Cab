ddbacb73f29c66a621fe145c55a57bae26f21e70
namangupta08 <guptanaman032@gmail.com>
2025-01-06 23:00:06 +0530

    Db setup

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose')
 
 function connecttodb()  {
-    
+    mongoose.connect(process.env.DB_CONNECT , {
+        useNewUrlParser:true,
+        useUnifiesTopology:true,
+    }).then (  () => {
+        console.log("Connected to DB")
+    }).catch(err => console.log("Could not connect to DB"))
 }
+
+module.exports = connecttodb;
