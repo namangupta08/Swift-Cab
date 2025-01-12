53d14bbc2e41f6c1e9b6afc5206e2219f70e6361
namangupta08 <guptanaman032@gmail.com>
2025-01-12 20:15:06 +0530

    userroute added

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,13 +1,20 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const {body} = require('express-validator')
-const {registerUser} = require('../controllers/UserController')
+const { body } = require("express-validator");
+const { registerUser } = require("../controllers/UserController");
 
-router.post('/register' , [
-    body('email').isEmail().withMessage('Inavlid email'),
-    body('fullname.firstname').isLength({min:3}).withMessage('First name must be atleast 3 character long'),
-    body('password').isLength({min:6}).withMessage('Password must be atleast 3 character long')
-
-] , registerUser)
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Inavlid email"),
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First name must be atleast 3 character long"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be atleast 3 character long"),
+  ],
+  registerUser
+);
 
 module.exports = router;
